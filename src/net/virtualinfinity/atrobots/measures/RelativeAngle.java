34267e206b53e1cbb5921f3cd4dfbcd9b172c46f
darkaura <darkaura@6e7d2f3c-e963-dc11-bff3-0008a12391b6>
2009-12-20 07:57:30 +0000

    Add slightly optimized exact bygree representation.

--- src/net/virtualinfinity/atrobots/measures/RelativeAngle.java
+++ src/net/virtualinfinity/atrobots/measures/RelativeAngle.java
@@ -4,7 +4,19 @@ package net.virtualinfinity.atrobots.measures;
  * @author Daniel Pitts
  */
 public class RelativeAngle implements Comparable<RelativeAngle> {
-    public static final RelativeAngle HALF_CIRCLE = RelativeAngle.fromRadians(Math.PI);
+    public static final RelativeAngle HALF_CIRCLE;
+    public static final RelativeAngle FULL_CIRCLE;
+    private static final RelativeAngle[] bygreeTable = new RelativeAngle[256];
+
+    static {
+        for (int bygree = 0; bygree < 256; ++bygree) {
+            final double radians = RelativeAngle.bygreesToRadians(bygree);
+            bygreeTable[bygree] = new RelativeBygreeAngle(bygree, (byte) bygree, radians, radiansToDegrees(radians));
+        }
+        HALF_CIRCLE = RelativeAngle.fromBygrees(128);
+        FULL_CIRCLE = RelativeAngle.fromBygrees(256);
+    }
+
     private final double radians;
 
     private RelativeAngle(double radians) {
@@ -40,7 +52,23 @@ public class RelativeAngle implements Comparable<RelativeAngle> {
     }
 
     public static RelativeAngle fromBygrees(int value) {
-        return fromRadians(value * Math.PI / 128);
+        if (value <= 255) {
+            if (value >= 0) {
+                return bygreeTable[value];
+            }
+            if (value >= -255) {
+                return bygreeTable[-value].negate();
+            }
+        }
+        return new RelativeBygreeAngle(value, (byte) value, bygreesToRadians(value), radiansToDegrees(value));
+    }
+
+    private RelativeAngle negate() {
+        return fromRadians(-radians);
+    }
+
+    private static double bygreesToRadians(int value) {
+        return value * Math.PI / 128;
     }
 
     public byte getSignedBygrees() {
@@ -52,10 +80,51 @@ public class RelativeAngle implements Comparable<RelativeAngle> {
     }
 
     public int compareTo(RelativeAngle angle) {
-        return Double.valueOf(getRadians()).compareTo(angle.getRadians());
+        return Double.compare(getRadians(), angle.getRadians());
     }
 
     public double getDegrees() {
-        return getRadians() * 180 / Math.PI;
+        return radiansToDegrees(getRadians());
+    }
+
+    private static double radiansToDegrees(double radians) {
+        return radians * 180 / Math.PI;
+    }
+
+    public double dividedBy(RelativeAngle relativeAngle) {
+        return radians / relativeAngle.radians;
+    }
+
+    private static class RelativeBygreeAngle extends RelativeAngle {
+        private final int bygree;
+        private final byte signedBygree;
+        private final double degrees;
+
+        public RelativeBygreeAngle(int bygree, byte signedBygree, double radians, double degrees) {
+            super(radians);
+            this.bygree = bygree;
+            this.signedBygree = signedBygree;
+            this.degrees = degrees;
+        }
+
+        @Override
+        public RelativeAngle normalize() {
+            return bygreeTable[bygree & 255];
+        }
+
+        @Override
+        public int getBygrees() {
+            return bygree;
+        }
+
+        @Override
+        public byte getSignedBygrees() {
+            return signedBygree;
+        }
+
+        @Override
+        public double getDegrees() {
+            return degrees;
+        }
     }
 }
