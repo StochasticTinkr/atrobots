11882db316a4cb85b8c7498bc41246882e61d578
pittsd <pittsd@6e7d2f3c-e963-dc11-bff3-0008a12391b6>
2010-01-05 00:13:33 +0000

    Improve "Memory" performance.

--- src/net/virtualinfinity/atrobots/computer/Memory.java
+++ src/net/virtualinfinity/atrobots/computer/Memory.java
@@ -8,16 +8,26 @@ import java.util.List;
  */
 public class Memory {
     private final List<MemoryArray> arrays = new ArrayList<MemoryArray>();
-    private static final DecrementMemoryOperation DECREMENT = new DecrementMemoryOperation();
-    private static final IncrementMemoryOperation INCREMENT = new IncrementMemoryOperation();
     private ComputerErrorHandler errorHandler;
 
     public void or(int index, short value) {
-        doOperation(index, new OrMemoryOperation(value));
+        for (MemoryArray array : arrays) {
+            if (index < array.size()) {
+                array.or(index, value);
+                return;
+            }
+            index -= array.size();
+        }
     }
 
     public void and(int index, short value) {
-        doOperation(index, new AndMemoryOperation(value));
+        for (MemoryArray array : arrays) {
+            if (index < array.size()) {
+                array.and(index, value);
+                return;
+            }
+            index -= array.size();
+        }
     }
 
     public ComputerErrorHandler getErrorHandler() {
@@ -32,87 +42,19 @@ public class Memory {
         return size;
     }
 
-    private interface MemoryOperation {
-        int perform(MemoryArray array, int index);
-    }
-
-    private static class SetMemoryOperation implements MemoryOperation {
-
-        final short value;
-
-        private SetMemoryOperation(short value) {
-            this.value = value;
-        }
-
-        public int perform(MemoryArray array, int index) {
-            array.put(index, value);
-            return 0;
-        }
-
-    }
-
-    private static class OrMemoryOperation implements MemoryOperation {
-
-        final short value;
-
-        private OrMemoryOperation(short value) {
-            this.value = value;
-        }
-
-        public int perform(MemoryArray array, int index) {
-            array.or(index, value);
-            return 0;
-        }
-
-    }
-
-    private static class AndMemoryOperation implements MemoryOperation {
-
-        final short value;
-
-        private AndMemoryOperation(short value) {
-            this.value = value;
-        }
-
-        public int perform(MemoryArray array, int index) {
-            array.and(index, value);
-            return 0;
-        }
-
-    }
-
-    private static class GetMemoryOperation implements MemoryOperation {
-
-        public int perform(MemoryArray array, int index) {
-            return array.get(index);
-        }
-
-    }
-
-    private static class DecrementMemoryOperation implements MemoryOperation {
-
-        public int perform(MemoryArray array, int index) {
-            array.decrement(index);
-            return 0;
-        }
-
-    }
-
-    private static class IncrementMemoryOperation implements MemoryOperation {
-
-        public int perform(MemoryArray array, int index) {
-            array.increment(index);
-            return 0;
-        }
-    }
-
     public void addMemoryArray(MemoryArray array) {
         arrays.add(array);
         array.setErrorHandler(errorHandler);
     }
 
     public short get(int index) {
-        return (short) doOperation(index, new GetMemoryOperation());
+        for (MemoryArray array : arrays) {
+            if (index < array.size()) {
+                return array.get(index);
+            }
+            index -= array.size();
+        }
+        return 0;
     }
 
     public int unsigned(int index) {
@@ -120,17 +62,13 @@ public class Memory {
     }
 
     public void set(int index, short value) {
-        doOperation(index, new SetMemoryOperation(value));
-    }
-
-    private int doOperation(int index, MemoryOperation operation) {
         for (MemoryArray array : arrays) {
             if (index < array.size()) {
-                return operation.perform(array, index);
+                array.put(index, value);
+                return;
             }
             index -= array.size();
         }
-        return 0;
     }
 
     public MemoryCell getCell(int index) {
@@ -138,11 +76,23 @@ public class Memory {
     }
 
     public void decrement(int index) {
-        doOperation(index, DECREMENT);
+        for (MemoryArray array : arrays) {
+            if (index < array.size()) {
+                array.decrement(index);
+                return;
+            }
+            index -= array.size();
+        }
     }
 
     public void increment(int index) {
-        doOperation(index, INCREMENT);
+        for (MemoryArray array : arrays) {
+            if (index < array.size()) {
+                array.increment(index);
+                return;
+            }
+            index -= array.size();
+        }
     }
 
     public void setErrorHandler(ComputerErrorHandler errorHandler) {
