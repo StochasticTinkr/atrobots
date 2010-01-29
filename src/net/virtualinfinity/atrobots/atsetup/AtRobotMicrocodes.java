4c9d5943023856585bebcd3c54b463faff1ae09e
pittsd <pittsd@6e7d2f3c-e963-dc11-bff3-0008a12391b6>
2010-01-29 23:07:29 +0000

    Clean up code.

--- src/net/virtualinfinity/atrobots/atsetup/AtRobotMicrocodes.java
+++ src/net/virtualinfinity/atrobots/atsetup/AtRobotMicrocodes.java
@@ -6,10 +6,10 @@ package net.virtualinfinity.atrobots.atsetup;
  * @author Daniel Pitts
  */
 public class AtRobotMicrocodes {
-    public static final short NUMBERED_LABEL = (short) 2;
-    public static final short REFERENCE = (short) 1;
-    public static final short CONSTANT = (short) 0;
-    public static final short RESOLVED_LABEL = (short) 4;
-    public static final short INDIRECT_REFERENCE_MASK = (short) 8;
-    public static final int UNRESOLVED_LABEL = 3;
+    public static final short CONSTANT = 0;
+    public static final short REFERENCE = 1;
+    public static final short NUMBERED_LABEL = 2;
+    public static final short UNRESOLVED_LABEL = 3;
+    public static final short RESOLVED_LABEL = 4;
+    public static final short INDIRECT_REFERENCE_MASK = 8;
 }
