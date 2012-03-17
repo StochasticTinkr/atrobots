f8b0693066f71b08ae9af04a41415e967bd05097
pittsd <pittsd@6e7d2f3c-e963-dc11-bff3-0008a12391b6>
2012-03-17 21:32:30 +0000

    Special cases for exact numbers.

--- testsrc/net/virtualinfinity/atrobots/measures/AngleTest.java
+++ testsrc/net/virtualinfinity/atrobots/measures/AngleTest.java
@@ -24,19 +24,21 @@ public class AngleTest extends TestCase {
     public static final double EAST_RADIANS = 0;
     public static final double SOUTH_RADIANS = PI / 2;
     public static final double WEST_RADIANS = PI;
+    private static final double BYGREE_DELTA = 0;
+    private static final double RADIAN_DELTA = 1.8369701987210297E-16;
 
     public void testBygreeOrientation() {
-        assertEquals(NORTH_SINE, AbsoluteAngle.fromBygrees(NORTH_BYGREES).sine(), 0.00001);
-        assertEquals(NORTH_COSINE, AbsoluteAngle.fromBygrees(NORTH_BYGREES).cosine(), 0.00001);
+        assertEquals(NORTH_SINE, AbsoluteAngle.fromBygrees(NORTH_BYGREES).sine(), BYGREE_DELTA);
+        assertEquals(NORTH_COSINE, AbsoluteAngle.fromBygrees(NORTH_BYGREES).cosine(), BYGREE_DELTA);
 
-        assertEquals(EAST_SINE, AbsoluteAngle.fromBygrees(EAST_BYGREES).sine(), 0.00001);
-        assertEquals(EAST_COSINE, AbsoluteAngle.fromBygrees(EAST_BYGREES).cosine(), 0.00001);
+        assertEquals(EAST_SINE, AbsoluteAngle.fromBygrees(EAST_BYGREES).sine(), BYGREE_DELTA);
+        assertEquals(EAST_COSINE, AbsoluteAngle.fromBygrees(EAST_BYGREES).cosine(), BYGREE_DELTA);
 
-        assertEquals(SOUTH_SINE, AbsoluteAngle.fromBygrees(SOUTH_BYGREES).sine(), 0.00001);
-        assertEquals(SOUTH_COSINE, AbsoluteAngle.fromBygrees(SOUTH_BYGREES).cosine(), 0.00001);
+        assertEquals(SOUTH_SINE, AbsoluteAngle.fromBygrees(SOUTH_BYGREES).sine(), BYGREE_DELTA);
+        assertEquals(SOUTH_COSINE, AbsoluteAngle.fromBygrees(SOUTH_BYGREES).cosine(), BYGREE_DELTA);
 
-        assertEquals(WEST_SINE, AbsoluteAngle.fromBygrees(WEST_BYGREES).sine(), 0.00001);
-        assertEquals(WEST_COSINE, AbsoluteAngle.fromBygrees(WEST_BYGREES).cosine(), 0.00001);
+        assertEquals(WEST_SINE, AbsoluteAngle.fromBygrees(WEST_BYGREES).sine(), BYGREE_DELTA);
+        assertEquals(WEST_COSINE, AbsoluteAngle.fromBygrees(WEST_BYGREES).cosine(), BYGREE_DELTA);
     }
 
     public void testFromBygrees() {
@@ -47,14 +49,14 @@ public class AngleTest extends TestCase {
     }
 
     public void testRadianOrientation() {
-        assertEquals(NORTH_SINE, AbsoluteAngle.fromRadians(NORTH_RADIANS).sine(), 0.00001);
-        assertEquals(NORTH_COSINE, AbsoluteAngle.fromRadians(NORTH_RADIANS).cosine(), 0.00001);
-        assertEquals(EAST_SINE, AbsoluteAngle.fromRadians(EAST_RADIANS).sine(), 0.00001);
-        assertEquals(EAST_COSINE, AbsoluteAngle.fromRadians(EAST_RADIANS).cosine(), 0.00001);
-        assertEquals(SOUTH_SINE, AbsoluteAngle.fromRadians(SOUTH_RADIANS).sine(), 0.00001);
-        assertEquals(SOUTH_COSINE, AbsoluteAngle.fromRadians(SOUTH_RADIANS).cosine(), 0.00001);
-        assertEquals(WEST_SINE, AbsoluteAngle.fromRadians(WEST_RADIANS).sine(), 0.00001);
-        assertEquals(WEST_COSINE, AbsoluteAngle.fromRadians(WEST_RADIANS).cosine(), 0.00001);
+        assertEquals(NORTH_SINE, AbsoluteAngle.fromRadians(NORTH_RADIANS).sine(), RADIAN_DELTA);
+        assertEquals(NORTH_COSINE, AbsoluteAngle.fromRadians(NORTH_RADIANS).cosine(), RADIAN_DELTA);
+        assertEquals(EAST_SINE, AbsoluteAngle.fromRadians(EAST_RADIANS).sine(), RADIAN_DELTA);
+        assertEquals(EAST_COSINE, AbsoluteAngle.fromRadians(EAST_RADIANS).cosine(), RADIAN_DELTA);
+        assertEquals(SOUTH_SINE, AbsoluteAngle.fromRadians(SOUTH_RADIANS).sine(), RADIAN_DELTA);
+        assertEquals(SOUTH_COSINE, AbsoluteAngle.fromRadians(SOUTH_RADIANS).cosine(), RADIAN_DELTA);
+        assertEquals(WEST_SINE, AbsoluteAngle.fromRadians(WEST_RADIANS).sine(), RADIAN_DELTA);
+        assertEquals(WEST_COSINE, AbsoluteAngle.fromRadians(WEST_RADIANS).cosine(), RADIAN_DELTA);
     }
 
     public void testGetAngleClockwiseTo() {
